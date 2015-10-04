57ced82c4f0b0c37f25b42298937ef14f0082c3b
haukepribnow <hauke.pribnow@gmx.net>
2015-10-04 11:56:12 +0200

    Fixed Medion X10 RF keymap/keytable
    After studying both the USB HID Usage Tables and some other remote control drivers I came to the realization that the keytable for the Medion X10 RF is not quite correct. In this commit I changed the keytable so that the remote buttons will match the descriptions of the USB HID Usage Tables as closely as possible. Where in doubt I also checked other remote control drivers to find out how the buttons are assigned to KEY_* constants there in order to get a assignments that match the style of other remotes better.

--- drivers/media/rc/keymaps/rc-medion-x10.c
+++ drivers/media/rc/keymaps/rc-medion-x10.c
@@ -25,70 +25,78 @@
 #include <media/rc-map.h>
 
 static struct rc_map_table medion_x10[] = {
-	{ 0x2c, KEY_TV },    /* TV */
-	{ 0x2d, KEY_VCR },   /* VCR */
-	{ 0x04, KEY_DVD },   /* DVD */
-	{ 0x06, KEY_AUDIO }, /* MUSIC */
+/*	{ code, key },                 button label or icon[, supertitle or subtitle] */
+	{ 0x2c, KEY_TV },              /* TV */
+	{ 0x2d, KEY_VCR },             /* VCR */
+	{ 0x04, KEY_DVD },             /* DVD */
+	{ 0x06, KEY_AUDIO },           /* MUSIC */
 
-	{ 0x2e, KEY_RADIO },     /* RADIO */
-	{ 0x05, KEY_DIRECTORY }, /* PHOTO */
-	{ 0x2f, KEY_INFO },      /* TV-PREVIEW */
-	{ 0x30, KEY_LIST },      /* CHANNEL-LST */
+	{ 0x2e, KEY_RADIO },           /* RADIO */
+	{ 0x05, KEY_IMAGES },          /* PHOTO */
+	{ 0x2f, KEY_PROGRAM },         /* <icon with a 3x3 grid of small TV screens>, subtitle: TV PREVIEW */
+	{ 0x30, KEY_CHANNEL },         /* <icon with several horizontal lines>, subtitle: CHANNEL LIST */
 
-	{ 0x1b, KEY_SETUP }, /* SETUP */
-	{ 0x31, KEY_VIDEO }, /* VIDEO DESKTOP */
+	{ 0x1b, KEY_CONFIG },          /* S, subtitle: SETUP */
+	{ 0x31, KEY_VIDEO },           /* <icon with a TV screen displaying lines>, subtitle: VIDEO DESKTOP */
 
-	{ 0x08, KEY_VOLUMEDOWN },  /* VOL - */
-	{ 0x09, KEY_VOLUMEUP },    /* VOL + */
-	{ 0x0b, KEY_CHANNELUP },   /* CHAN + */
-	{ 0x0c, KEY_CHANNELDOWN }, /* CHAN - */
-	{ 0x00, KEY_MUTE },        /* MUTE */
+	{ 0x08, KEY_VOLUMEDOWN },      /* VOL - */
+	{ 0x09, KEY_VOLUMEUP },        /* VOL + */
+	{ 0x0b, KEY_CHANNELUP },       /* CHAN + */
+	{ 0x0c, KEY_CHANNELDOWN },     /* CHAN - */
+	{ 0x00, KEY_MUTE },            /* <Mute icon> */
 
-	{ 0x32, KEY_RED }, /* red */
-	{ 0x33, KEY_GREEN }, /* green */
-	{ 0x34, KEY_YELLOW }, /* yellow */
-	{ 0x35, KEY_BLUE }, /* blue */
-	{ 0x16, KEY_TEXT }, /* TXT */
+	{ 0x32, KEY_RED },             /* <red circle> */
+	{ 0x33, KEY_GREEN },           /* <green circle> */
+	{ 0x34, KEY_YELLOW },          /* <yellow circle> */
+	{ 0x35, KEY_BLUE },            /* <blue circle> */
+	{ 0x16, KEY_TEXT },            /* TXT */
 
-	{ 0x0d, KEY_1 },
-	{ 0x0e, KEY_2 },
-	{ 0x0f, KEY_3 },
-	{ 0x10, KEY_4 },
-	{ 0x11, KEY_5 },
-	{ 0x12, KEY_6 },
-	{ 0x13, KEY_7 },
-	{ 0x14, KEY_8 },
-	{ 0x15, KEY_9 },
-	{ 0x17, KEY_0 },
-	{ 0x1c, KEY_SEARCH }, /* TV/RAD, CH SRC */
-	{ 0x20, KEY_DELETE }, /* DELETE */
+	{ 0x0d, KEY_NUMERIC_1 },
+	{ 0x0e, KEY_NUMERIC_2 },
+	{ 0x0f, KEY_NUMERIC_3 },
+	{ 0x10, KEY_NUMERIC_4 },
+	{ 0x11, KEY_NUMERIC_5 },
+	{ 0x12, KEY_NUMERIC_6 },
+	{ 0x13, KEY_NUMERIC_7 },
+	{ 0x14, KEY_NUMERIC_8 },
+	{ 0x15, KEY_NUMERIC_9 },
+	{ 0x17, KEY_NUMERIC_0 },
+	{ 0x1c, KEY_SEARCH },          /* TV/RADIO, supertitle: CH. SEARCH */
+	{ 0x20, KEY_BACKSPACE },       /* <arrow pointing to the left>, supertitle: DELETE */
 
-	{ 0x36, KEY_KEYBOARD }, /* RENAME */
-	{ 0x18, KEY_SCREEN },   /* SNAPSHOT */
+	{ 0x36, KEY_EDIT },            /* <empty>, supertitle: RENAME */
+	        /* KEY_EDIT seemed to be the closest match for "RENAME" */
+	{ 0x18, KEY_SYSRQ },           /* <empty>, supertitle: SNAPSHOT */
+	        /* KEY_SYSRQ (being the button to create screenshots) seemed to be the closest
+	           match for SNAPSHOT. Also, KEY_SYSRQ is also used by other remotes for a
+	           button with such a label, e.g. kworld_pc150u or nvec. */
 
-	{ 0x1a, KEY_UP },    /* up */
-	{ 0x22, KEY_DOWN },  /* down */
-	{ 0x1d, KEY_LEFT },  /* left */
-	{ 0x1f, KEY_RIGHT }, /* right */
-	{ 0x1e, KEY_OK },    /* OK */
+	{ 0x1a, KEY_UP },              /* <triangle pointing up> */
+	{ 0x22, KEY_DOWN },            /* <triangle pointing down> */
+	{ 0x1d, KEY_LEFT },            /* <triangle pointing left> */
+	{ 0x1f, KEY_RIGHT },           /* <triangle pointing right> */
+	{ 0x1e, KEY_OK },              /* OK */
 
-	{ 0x37, KEY_SELECT }, /* ACQUIRE IMAGE */
-	{ 0x38, KEY_EDIT },   /* EDIT IMAGE */
+	{ 0x37, KEY_CAMERA },          /* <empty>, supertitle: ACQUIRE IMAGE */
+	{ 0x38, KEY_GRAPHICSEDITOR },  /* <empty>, supertitle: EDIT IMAGE */
 
-	{ 0x24, KEY_REWIND },   /* rewind  (<<) */
-	{ 0x25, KEY_PLAY },     /* play    ( >) */
-	{ 0x26, KEY_FORWARD },  /* forward (>>) */
-	{ 0x27, KEY_RECORD },   /* record  ( o) */
-	{ 0x28, KEY_STOP },     /* stop    ([]) */
-	{ 0x29, KEY_PAUSE },    /* pause   ('') */
+	{ 0x24, KEY_REWIND },          /* <rewind icon>       (<<) */
+	{ 0x25, KEY_PLAY },            /* <play icon>         ( >) */
+	{ 0x26, KEY_FASTFORWARD },     /* <fast forward icon> (>>) */
+	{ 0x27, KEY_RECORD },          /* <record icon>       ( o) */
+	{ 0x28, KEY_STOP },            /* <stop icon>         ([]) */
+	{ 0x29, KEY_PAUSE },           /* <pause icon>        (||) */
 
-	{ 0x21, KEY_PREVIOUS },        /* prev */
-	{ 0x39, KEY_SWITCHVIDEOMODE }, /* F SCR */
-	{ 0x23, KEY_NEXT },            /* next */
-	{ 0x19, KEY_MENU },            /* MENU */
-	{ 0x3a, KEY_LANGUAGE },        /* AUDIO */
+	{ 0x21, KEY_PREVIOUS },        /* <skip to prev icon> (|<<) */
+	{ 0x39, KEY_ZOOM },            /* <icon with an empty TV screen>, subtitle: FULL SCREEN */
+	{ 0x23, KEY_NEXT },            /* <skip to next icon> (>>|)  */
+	
+	{ 0x19, KEY_MENU },            /* <empty>, subtitle: DVD MENU */
+	{ 0x3a, KEY_LANGUAGE },        /* <empty>, subtitle: DVD AUDIO */
 
-	{ 0x02, KEY_POWER }, /* POWER */
+	{ 0x02, KEY_POWER2 },          /* <power icon> */
+	        /* Other remotes use KEY_POWER2 (e.g. kworld_pc150u or nvec) instead of KEY_POWER,
+	           so let's do this here as well. */
 };
 
 static struct rc_map_list medion_x10_map = {
